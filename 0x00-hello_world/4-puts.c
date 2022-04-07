5cca1709f11adcf98e6538fcd9371c1e6574ff38
okeyifea <ifeanyiokey98@gmail.com>
2022-04-07 08:11:39 -0700

    mmj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  * main - entry point
  * 
@@ -6,6 +6,6 @@ include <stdio.h>
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
