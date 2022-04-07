bea3e942c51f708e4bca753ed76d3a75198c5045
okeyifea <ifeanyiokey98@gmail.com>
2022-04-07 10:41:12 -0700

    mmj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,5 @@ printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));	
 printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+return (0);
 }
