26953e0f9723ad683fcfde58ef934132e8ed63ff
okeyifea <ifeanyiokey98@gmail.com>
2022-04-08 11:34:03 -0700

    hhh

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return 0 (success/correct)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+	printf("%digit", digit);
+	}
+
+	printf('\n');
+
+	return (0);
+}
