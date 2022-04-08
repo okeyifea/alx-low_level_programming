c2340dfd69f90fffce3ce5b42a052d0a2ffb356f
okeyifea <ifeanyiokey98@gmail.com>
2022-04-08 11:03:19 -0700

    mms

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+	{
+		if (alphabet == 'q')
+			continue;
+		else if (alphabet == 'e')
+			continue;
+
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
