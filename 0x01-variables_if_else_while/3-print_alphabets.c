fe5132e42d08fcaceffa30f3f50bc68eb71d69d1
okeyifea <ifeanyiokey98@gmail.com>
2022-04-08 10:36:52 -0700

    hhh

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,14 @@ int main(void)
 	char upper_case;
 	char lower_case;
 
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-	{
-		putchar(upper_case);
-	}
-
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
 		putchar(lower_case);
+	}
+
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	{
+		putchar(upper_case);
 	}
 
 	putchar('\n');
