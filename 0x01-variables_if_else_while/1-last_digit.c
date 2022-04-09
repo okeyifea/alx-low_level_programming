6dd25cfdd5b9805f62684220590d17db4ebc850b
okeyifea <ifeanyiokey98@gmail.com>
2022-04-09 04:40:29 -0700

    mms

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 
 	else
 	{
-		printf("last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
+		printf("last digit of %i is %i and is less than 6 and not 0\n",                 n, n % 10);
 	}
 
 	return (0);
