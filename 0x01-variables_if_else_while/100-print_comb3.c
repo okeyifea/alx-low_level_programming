a650d81d6b1f5e4a504b5fcd6c4c5a54cc660f0d
okeyifea <ifeanyiokey98@gmail.com>
2022-04-11 06:24:27 -0700

    hhh

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-  * main - Prints numbers between 00 to 89.
+  * main - Entry point
   *
   * Return: Always 0 (Success)
   */
+
 int main(void)
 {
 	int i, e;
@@ -23,7 +24,7 @@ int main(void)
 					break;
 				}
 					putchar(',');
-						putchar(' ');
+					putchar(' ');
 			}
 			i++;
 		}
