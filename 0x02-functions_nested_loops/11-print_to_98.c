81d51911a96eafcf6e127b3a224437429a51e5de
okeyifea <ifeanyiokey98@gmail.com>
2022-04-12 14:29:27 -0700

    jjj

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,15 @@ void print_to_98(int n)
 
 	while (n < 98)
 	{
-		printf("%i, ",n);
+		printf("%i, ", n);
 		n++;
 	}
 	while (n > 98)
 	{
-		printf("%i, ",n);
+		printf("%i, ", n);
 		n--;
 	}
 
 	printf("98");
 	putchar('\n');
 }
-
