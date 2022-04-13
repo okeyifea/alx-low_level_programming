06931e4a21494a5f59c2161d950916c3f8f44551
okeyifea <ifeanyiokey98@gmail.com>
2022-04-13 15:13:13 -0700

    ggg

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,7 @@ int main(void)
 	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 + fib2;
-		printf("%lu,", sum);
+		printf("%lu", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
@@ -34,7 +34,7 @@ int main(void)
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-		printf("%ld", half1, half2);
+		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(",");
 
