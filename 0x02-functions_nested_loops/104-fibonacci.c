c4d56fb59d018da286770c382db3ea74ebb66abb
okeyifea <ifeanyiokey98@gmail.com>
2022-04-13 15:16:40 -0700

    eer

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -29,7 +29,7 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-		if(fib1_half2 + fib2_half2 > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
