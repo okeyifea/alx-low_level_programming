35eee62ed6db063a354fa937f8f4a4f3f04b406d
okeyifea <ifeanyiokey98@gmail.com>
2022-04-13 12:58:20 -0700

    fff

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,45 @@
 #include "main.h"
 
 /**
- * print_times_table - print times table
- * @n: times table to use
- * Return: void
+ * print_times_table - print the times table of the input,
+ * @n: the value of the times table to be printed
+ * Return: Always 0
  */
 
 void print_times_table(int n)
 {
-	int a = 0, rep, b;
+	int num, mult, prod;
 	
-	if (n < 0 || n > 15)
-		return;
-
-	while (a <= n)
+	if (n >= 0 && n <= 15)
 	{
-		for (b = 0; b <=n; b++)
+		for (num = 0; num <= n; num++)
 		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
-			else if (rep < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) /10);
-				_putchar('0' + (rep % 10);
-						}
-						if (b < n)
-						{
-						_putchar(',');
-						_putchar(' ');
-						}
-						}
-						_putchar('\n');'
-						a++;
-						}
-						}
+			_putchar('\n');
+		}
+	}
+}
