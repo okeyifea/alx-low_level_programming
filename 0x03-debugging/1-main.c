8abb3a0d741fc710b0f728d98867852cdbde7c02
okeyifea <ifeanyiokey98@gmail.com>
2022-04-14 13:35:14 -0700

    bad

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
+*
 * Return: 0
 */
-
 int main(void)
 {
         int i;
@@ -16,10 +15,10 @@ int main(void)
 	/*
 	 * while (i < 10)
 	 * {
+	 * infinite loop - No increase of variable
 	 * putchar(i);
-	 * }
+	 }
 	 */
         printf("Infinite loop avoided! \\o/\n");
-
         return (0);
 }
