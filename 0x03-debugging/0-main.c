37a4b6550a773b03a3a09b73577f2c56e1d7a1a3
okeyifea <ifeanyiokey98@gmail.com>
2022-04-14 00:34:04 -0700

    nnn

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * main - test function that print if integer positive or negative
+ * main - test function that print if integer is positive or negative
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
