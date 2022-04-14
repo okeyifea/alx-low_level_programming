3c08a5f8620c2a316d99e1bf0e39579cdfaa88ac
okeyifea <ifeanyiokey98@gmail.com>
2022-04-13 23:58:48 -0700

    hhh

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
+
 #include "main.h"
 
 /**
- * main - main
+ * main - test function that print if integer positive or negative
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	postive_or_negative(i);
 
 	return (0);
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,6 @@
 
 #include <stdio.h>
 
-void postive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+void positive_or_negative(int i);
 
-#endif
+#endif /* MAIN_H */
