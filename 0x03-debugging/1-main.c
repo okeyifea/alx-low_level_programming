1cc348cdd15d222ae0f7d3178e5e5cdc0e3c32db
okeyifea <ifeanyiokey98@gmail.com>
2022-04-14 12:58:02 -0700

    huge

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,10 +15,11 @@ int main(void)
 
 	/*
 	 * while (i < 10)
-	 {
+	 * {
 	 * putchar(i);
 	 * }
 	 */
         printf("Infinite loop avoided! \\o/\n");
+
         return (0);
 }
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,21 +8,25 @@
  * Return: largest number
  */
 
-int laegest_number(int a, int b, int c)
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > c && b > a)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b)
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = b;
 	}
 
 	return (largest);
