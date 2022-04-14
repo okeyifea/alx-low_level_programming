63cc57c5b2d39fa7ace707c1a712d254bbb5b1e7
okeyifea <ifeanyiokey98@gmail.com>
2022-04-13 23:16:27 -0700

    mss

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,14 +13,12 @@ int main(void)
 
 	i = 0;
 
-	/*
-	 * while (i < 10)
-	 * {
-	 * indefinite loop - no increase of variables
-	 * putchar (i);
-	 * }
-	 */
+	 while (i < 10)
+	 {
+		 putchar(i);
+	 }
 
 	printf("infinite loop avoided! \\o/\n");
+
 	return (0);
 }
--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -2,14 +2,13 @@
 
 /**
  * main - main
- *
  * Return: 0
  */
 int main(void)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	postive_or_negative (i);
 
 	return (0);
