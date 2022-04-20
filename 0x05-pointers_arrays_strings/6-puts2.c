22f67ec165203d55a75e18304a8437e48446433a
okeyifea <ifeanyiokey98@gmail.com>
2022-04-20 05:59:40 -0700

    red

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,7 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		if(i % 2 == 0)
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 		len++;
 	}
 
-	if(len % 2 == 0)
+	if (len % 2 == 0)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
