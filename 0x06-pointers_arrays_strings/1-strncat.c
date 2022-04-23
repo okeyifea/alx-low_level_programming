5c61dfbb97441669e30ff2fc3cb2078e54f8e06d
Okey ifeanyi <101342170+okeyifea@users.noreply.github.com>
2022-04-23 11:41:06 -0700

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,29 +4,28 @@
  *@dest: A pointer to a character that will be changed
  *@src: A pointer to a character that will be changed
  *@n: value
+ *
  *Return: dest
  */
-
 char *_strncat(char *dest, char *src, int n)
-
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
+	
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
 	j = 0;
-	while (j < n && src[j] != '\0')
+	
+	while (src[j] != 0 && j < n)
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
-	
-	dest{i} = '\0';
-
 	return (dest);
 }
