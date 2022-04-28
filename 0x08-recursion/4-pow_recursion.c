d26f991649459958720fdc326c2f42df44ee9151
okeyifea <ifeanyiokey98@gmail.com>
2022-04-28 08:01:25 -0700

    kite

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - check the code
+ * @n: the value of x is raise to the power of y
+ * Return: Always 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (0);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+}
