9d7ca37a74023459aa2dc8abe39c1d802155a739
okeyifea <ifeanyiokey98@gmail.com>
2022-04-28 07:21:23 -0700

    mey

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - check the code
+ * @s: length of string
+ * Return: Always 0
+ */
+int _strlen_recursion(char *s)
+{
+	char str[]= "_strlen_recursion";
+	printf("Length of string is: %d", strlen(str));
+	return 0;
+}
