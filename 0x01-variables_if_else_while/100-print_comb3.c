46c25c1d25792d3a0ba69d90886a3d1f96caa773
Okey ifeanyi <101342170+okeyifea@users.noreply.github.com>
2022-05-08 23:43:38 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,7 @@ int main(void)
 	int digit1, digit2;
 
 	for (digit1 = 0; digit1 < 9; digit1++)
-	{ 
+	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
@@ -27,8 +27,6 @@ int main(void)
 			putchar(' ');
 		}
  	}
-
 	putchar('\n');
-
 	return (0);
 }
