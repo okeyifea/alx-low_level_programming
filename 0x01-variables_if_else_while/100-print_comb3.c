7d68715af03bdc561a4b7c0978d58cebd5cabc46
Okey ifeanyi <101342170+okeyifea@users.noreply.github.com>
2022-05-08 23:45:25 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,9 @@
-/*
- * File: 100-print_comb3.c
- * Auth: Brennan D Baraban
- */
 #include <stdio.h>
+
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * main - shows all possible number
  *
- * Return: Always 0.
+ * Return: 0
  */
 int main(void)
 {
@@ -15,18 +11,16 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
- 	}
+	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+	{
+		putchar((digit1 % 10) + '0');
+		putchar((digit2 % 10) + '0');
+		if (digit1 == 8 && digit2 == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	}
 	putchar('\n');
 	return (0);
 }
