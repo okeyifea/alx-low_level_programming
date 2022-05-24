296f3d96bf1356609af39e97327894658c1828fd
Okey ifeanyi <101342170+okeyifea@users.noreply.github.com>
2022-05-24 10:16:33 -0700

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
